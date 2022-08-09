7bcb3b64035afdf2d57770d9d9cff756f307fe65
watelia <rom.hirtsk.20@gmail.com>
2022-08-09 17:53:50 +0900

    create message box

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,6 +10,19 @@
 @section('content')
     <p>ここが本文のコンテンツです</p>
     <p>必要なだけ記述できます</p>
+
+    {{-- @component('components.message')
+        @slot('msg_title')
+            CAUTION!
+        @endslot
+
+        @slot('msg_content')
+            これはメッセージの表示
+        @endslot
+    @endcomponent --}}
+
+    @include('components.message', ['msg_title' => 'OK', 'msg_content' => 'サブビューです'])
+
 @endsection
 
 @section('footer')
