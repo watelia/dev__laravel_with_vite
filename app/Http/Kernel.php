d800ce32912727b6a2ee5ed032e5b1aa6ba21f73
watelia <rom.hirtsk.20@gmail.com>
2022-08-17 13:36:01 +0900

    created global-middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,12 +36,17 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\HelloMiddleware::class,
         ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'helo' => [
+            \App\Http\Middleware\HelloMiddleware::class,
         ],
     ];
 
